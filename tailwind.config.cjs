69728d8ba35b981939e81294ad29ebe89927d1db
nictoro <nicholas.biantoro@ruangguru.com>
2022-05-03 17:51:13 +0700

    setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#8689e0',
+        'primaryHover': "#5a5fdb",
+        'secondary': '#76fddd',
+        'secondaryHover': "#30dfb6",
+        'success': '#76fddd',
+        'successHover': '#30dfb6',
+        'error': '#fe8977',
+        'errorHover': "#e96957"
+      }
+    },
+  },
+  plugins: [],
+}
---
+++ tests/e2e/test.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@playwright/test';
+
+test('about page has expected h1', async ({ page }) => {
+	await page.goto('/questions/ask');
+	expect(await page.textContent('h1')).toBe('Write A Question');
+});
